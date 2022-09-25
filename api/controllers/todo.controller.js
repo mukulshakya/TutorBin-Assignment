d8a094da9760626a99aa540b186c96129d262c6a
Mukul Shakya <muklshakya123@gmail.com>
2022-09-25 20:21:09 +0530

    Error handling

--- api/controllers/todo.controller.js
+++ api/controllers/todo.controller.js
@@ -2,109 +2,129 @@ const { uuid, isUuid } = require("uuidv4");
 const db = require("../../db");
 
 exports.createTodo = async (req, res) => {
-  const requiredKeys = ["todo", "dateTime"];
-  for (const key of requiredKeys) {
-    if (!req.body[key] || !req.body[key].trim())
-      return res.badRequest(`${key} is required`);
-    else req.body[key] = req.body[key].trim();
-  }
+  try {
+    const requiredKeys = ["todo", "dateTime"];
+    for (const key of requiredKeys) {
+      if (!req.body[key] || !req.body[key].trim())
+        return res.badRequest(`${key} is required`);
+      else req.body[key] = req.body[key].trim();
+    }
 
-  const { todo, dateTime } = req.body;
+    const { todo, dateTime } = req.body;
 
-  if (new Date(dateTime).toString() === "Invalid Date")
-    return res.badRequest("Invalid date time format");
-
-  const todoExists = db.todos.findOne({ todo: todo.toLowerCase() });
-  if (todoExists) return res.badRequest("Todo already exists");
-
-  db.todos.insert({
-    id: uuid(),
-    username: req.user.username,
-    todo: todo.toLowerCase(),
-    dateTime: new Date(dateTime),
-    status: "pending",
-    createdAt: new Date(),
-  });
-
-  return res.created("Todo created");
-};
-
-exports.getTodos = async (req, res) => {
-  const todos = db.todos
-    .find({})
-    .map(({ id, username, todo, dateTime, status, createdAt }) => ({
-      id,
-      username,
-      todo,
-      dateTime,
-      status,
-      createdAt,
-    }));
-
-  return res.ok(todos);
-};
-
-exports.getOneTodo = async (req, res) => {
-  const id = req.params.id;
-  if (!isUuid(id)) return res.badRequest("Invalid id");
-
-  const todo = db.todos.findOne({ id });
-  if (!todo) return res.notFound("Todo not found");
-
-  return res.ok({
-    id: todo.id,
-    username: todo.username,
-    todo: todo.todo,
-    dateTime: todo.dateTime,
-    status: todo.status,
-    createdAt: todo.createdAt,
-  });
-};
-
-exports.deleteTodo = async (req, res) => {
-  const id = req.params.id;
-  if (!isUuid(id)) return res.badRequest("Invalid id");
-
-  const todo = db.todos.findOne({ id });
-  if (!todo) return res.notFound("Todo not found");
-
-  db.todos.remove(todo);
-
-  return res.noContent();
-};
-
-exports.updateTodo = async (req, res) => {
-  const id = req.params.id;
-  if (!isUuid(id)) return res.badRequest("Invalid id");
-
-  const todoExist = db.todos.findOne({ id });
-  if (!todoExist) return res.notFound("Todo not found");
-
-  const allowedKeys = ["todo", "dateTime"];
-  for (const key of allowedKeys) {
-    if (req.body[key] && !req.body[key].trim())
-      return res.badRequest(`${key} is missing`);
-    else req.body[key] = req.body[key].trim();
-  }
-
-  const { todo, dateTime } = req.body;
-  if (todo) {
-    const nameTaken = db.todos.findOne({
-      todo: todo.toLowerCase(),
-      id: { $ne: id },
-    });
-    if (nameTaken) return res.badRequest("Todo already exists");
-
-    todoExist.todo = todo.toLowerCase();
-  }
-  if (dateTime) {
     if (new Date(dateTime).toString() === "Invalid Date")
       return res.badRequest("Invalid date time format");
 
-    todoExist.dateTime = new Date(dateTime);
+    const todoExists = db.todos.findOne({ todo: todo.toLowerCase() });
+    if (todoExists) return res.badRequest("Todo already exists");
+
+    db.todos.insert({
+      id: uuid(),
+      username: req.user.username,
+      todo: todo.toLowerCase(),
+      dateTime: new Date(dateTime),
+      status: "pending",
+      createdAt: new Date(),
+    });
+
+    return res.created("Todo created");
+  } catch (e) {
+    return res.unexpected();
   }
+};
 
-  db.todos.update(todoExist);
+exports.getTodos = async (req, res) => {
+  try {
+    const todos = db.todos
+      .find({})
+      .map(({ id, username, todo, dateTime, status, createdAt }) => ({
+        id,
+        username,
+        todo,
+        dateTime,
+        status,
+        createdAt,
+      }));
 
-  return res.noContent();
+    return res.ok(todos);
+  } catch (e) {
+    return res.unexpected();
+  }
+};
+
+exports.getOneTodo = async (req, res) => {
+  try {
+    const id = req.params.id;
+    if (!isUuid(id)) return res.badRequest("Invalid id");
+
+    const todo = db.todos.findOne({ id });
+    if (!todo) return res.notFound("Todo not found");
+
+    return res.ok({
+      id: todo.id,
+      username: todo.username,
+      todo: todo.todo,
+      dateTime: todo.dateTime,
+      status: todo.status,
+      createdAt: todo.createdAt,
+    });
+  } catch (e) {
+    return res.unexpected();
+  }
+};
+
+exports.deleteTodo = async (req, res) => {
+  try {
+    const id = req.params.id;
+    if (!isUuid(id)) return res.badRequest("Invalid id");
+
+    const todo = db.todos.findOne({ id });
+    if (!todo) return res.notFound("Todo not found");
+
+    db.todos.remove(todo);
+
+    return res.noContent();
+  } catch (e) {
+    return res.unexpected();
+  }
+};
+
+exports.updateTodo = async (req, res) => {
+  try {
+    const id = req.params.id;
+    if (!isUuid(id)) return res.badRequest("Invalid id");
+
+    const todoExist = db.todos.findOne({ id });
+    if (!todoExist) return res.notFound("Todo not found");
+
+    const allowedKeys = ["todo", "dateTime"];
+    for (const key of allowedKeys) {
+      if (req.body[key] && !req.body[key].trim())
+        return res.badRequest(`${key} is missing`);
+      else req.body[key] = req.body[key].trim();
+    }
+
+    const { todo, dateTime } = req.body;
+    if (todo) {
+      const nameTaken = db.todos.findOne({
+        todo: todo.toLowerCase(),
+        id: { $ne: id },
+      });
+      if (nameTaken) return res.badRequest("Todo already exists");
+
+      todoExist.todo = todo.toLowerCase();
+    }
+    if (dateTime) {
+      if (new Date(dateTime).toString() === "Invalid Date")
+        return res.badRequest("Invalid date time format");
+
+      todoExist.dateTime = new Date(dateTime);
+    }
+
+    db.todos.update(todoExist);
+
+    return res.noContent();
+  } catch (e) {
+    return res.unexpected();
+  }
 };
--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -6,45 +6,53 @@ const { uuid } = require("uuidv4");
 const saltRounds = 10;
 
 exports.signUp = async (req, res) => {
-  const requiredKeys = ["username", "password"];
-  for (const key of requiredKeys) {
-    if (!req.body[key] || !req.body[key].trim())
-      return res.badRequest(`${key} is required`);
-    else req.body[key] = req.body[key].trim();
+  try {
+    const requiredKeys = ["username", "password"];
+    for (const key of requiredKeys) {
+      if (!req.body[key] || !req.body[key].trim())
+        return res.badRequest(`${key} is required`);
+      else req.body[key] = req.body[key].trim();
+    }
+
+    const { username, password } = req.body;
+    const passwordPattern = /^[a-z0-9]{6,}$/i;
+
+    const usernameTaken = db.users.findOne({ username });
+    if (usernameTaken) return res.badRequest("Username is already taken");
+
+    if (!passwordPattern.test(password))
+      return res.badRequest("Password should be alphanumeric");
+
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+    db.users.insert({ id: uuid(), username, password: hashedPassword });
+
+    return res.created("Signup successfull");
+  } catch (e) {
+    return res.unexpected();
   }
-
-  const { username, password } = req.body;
-  const passwordPattern = /^[a-z0-9]{6,}$/i;
-
-  const usernameTaken = db.users.findOne({ username });
-  if (usernameTaken) return res.badRequest("Username is already taken");
-
-  if (!passwordPattern.test(password))
-    return res.badRequest("Password should be alphanumeric");
-
-  const hashedPassword = await bcrypt.hash(password, saltRounds);
-
-  db.users.insert({ id: uuid(), username, password: hashedPassword });
-
-  return res.created("Signup successfull");
 };
 
 exports.login = async (req, res) => {
-  const requiredKeys = ["username", "password"];
-  for (const key of requiredKeys) {
-    if (!req.body[key] || !req.body[key].trim())
-      return res.badRequest(`${key} is required`);
-    else req.body[key] = req.body[key].trim();
+  try {
+    const requiredKeys = ["username", "password"];
+    for (const key of requiredKeys) {
+      if (!req.body[key] || !req.body[key].trim())
+        return res.badRequest(`${key} is required`);
+      else req.body[key] = req.body[key].trim();
+    }
+
+    const { username, password } = req.body;
+
+    const user = db.users.findOne({ username });
+    if (!user) return res.notFound("user not found");
+
+    const match = await bcrypt.compare(password, user.password);
+    if (!match) return res.unAuth("Incorrect password");
+
+    const token = jwt.sign({ username }, process.env.JWT_SECRET);
+    return res.ok({ username, token }, "Login successfull");
+  } catch (e) {
+    return res.unexpected();
   }
-
-  const { username, password } = req.body;
-
-  const user = db.users.findOne({ username });
-  if (!user) return res.notFound("user not found");
-
-  const match = await bcrypt.compare(password, user.password);
-  if (!match) return res.unAuth("Incorrect password");
-
-  const token = jwt.sign({ username }, process.env.JWT_SECRET);
-  return res.ok({ username, token }, "Login successfull");
 };
