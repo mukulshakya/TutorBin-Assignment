1cee8601b71931a9d2ac32dab3fb4b9964f92a33
Mukul Shakya <muklshakya123@gmail.com>
2022-09-25 19:41:31 +0530

    Added get todo routes

--- api/controllers/todo.controller.js
+++ api/controllers/todo.controller.js
@@ -1,3 +1,4 @@
+const { uuid, isUuid } = require("uuidv4");
 const db = require("../../db");
 
 exports.createTodo = async (req, res) => {
@@ -13,13 +14,49 @@ exports.createTodo = async (req, res) => {
   if (new Date(dateTime).toString() === "Invalid Date")
     return res.badRequest("Invalid date time format");
 
+  const todoExists = db.todos.findOne({ todo: todo.toLowerCase() });
+  if (todoExists) return res.badRequest("Todo already exists");
+
   db.todos.insert({
+    id: uuid(),
     username: req.user.username,
-    todo,
+    todo: todo.toLowerCase(),
     dateTime: new Date(dateTime),
     status: "pending",
     createdAt: new Date(),
   });
 
   return res.created("Todo created");
+};
+
+exports.getTodos = async (req, res) => {
+  const todos = db.todos
+    .find({})
+    .map(({ id, username, todo, dateTime, status, createdAt }) => ({
+      id,
+      username,
+      todo,
+      dateTime,
+      status,
+      createdAt,
+    }));
+
+  return res.ok(todos);
+};
+
+exports.getOneTodo = async (req, res) => {
+  const id = req.params.id;
+  if (!isUuid(id)) return res.badRequest("Invalid id");
+
+  const todo = db.todos.findOne({ id });
+  if (!todo) return res.notFound("Todo not found");
+
+  return res.ok({
+    id: todo.id,
+    username: todo.username,
+    todo: todo.todo,
+    dateTime: todo.dateTime,
+    status: todo.status,
+    createdAt: todo.createdAt,
+  });
 };
--- api/routes/todo.route.js
+++ api/routes/todo.route.js
@@ -4,5 +4,7 @@ const controller = require("../controllers/todo.controller");
 const router = express.Router();
 
 router.post("/", controller.createTodo);
+router.get("/", controller.getTodos);
+router.get("/:id", controller.getOneTodo);
 
 module.exports = router;
